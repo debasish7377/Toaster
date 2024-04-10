5a41d510989d3e6dff73cc0b9982df4c728f1960
monosage <debasish_sahoo@monosage.com>
2024-04-10 12:25:41 +0530

    This is 44 commit

--- SToast/build.gradle.kts
+++ SToast/build.gradle.kts
@@ -52,7 +52,7 @@ afterEvaluate {
 
                 groupId = "com.github.debasish7377"
                 artifactId = "Toaster"
-                version = "2.4.4"
+                version = "2.4.5"
             }
         }
     }
